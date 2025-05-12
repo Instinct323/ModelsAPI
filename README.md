7edfb99e5832fd2ce806bd42ca4a33d789134603
tongzj <tongzanjia@qq.com>
2025-05-12 22:49:05 +0800

    add GroundingDINO

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 - depth_anything_v2: from [DepthAnything/Depth-Anything-V2](https://github.com/DepthAnything/Depth-Anything-V2)
 - open_clip: from [mlfoundations/open_clip](https://github.com/mlfoundations/open_clip)
 - sam2: from [facebookresearch/sam2](https://github.com/facebookresearch/sam2)
+
+# Leaderboard
+
+- [Chatbot](https://openlm.ai/chatbot-arena/)
+- [LLM Benchmark](https://livebench.ai/#/)
+- [Zero-Shot Object Detection](https://paperswithcode.com/sota/zero-shot-object-detection-on-mscoco)
---
+++ gdino.py
@@ -0,0 +1,55 @@
+import warnings
+
+warnings.filterwarnings("ignore")
+
+import cv2
+
+import groundingdino.datasets.transforms as T
+import numpy as np
+from PIL import Image
+
+from groundingdino.util.inference import load_model, predict, annotate
+
+transform = T.Compose([
+    T.RandomResize([800], max_size=1333),
+    T.ToTensor(),
+    T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+])
+
+
+class GDINO:
+
+    def __init__(self,
+                 box_thresh=0.3,
+                 text_thresh=0.25):
+        # pip install git+https://github.com/IDEA-Research/GroundingDINO.git
+        from groundingdino.config import GroundingDINO_SwinT_OGC as config
+        checkpoint = "checkpoints/groundingdino_swint_ogc.pth"
+        self.model = load_model(config.__file__, checkpoint)
+        self.box_thresh = box_thresh
+        self.text_thresh = text_thresh
+
+    def __call__(self,
+                 image: Image,
+                 prompt: str):
+        """ :return: boxes, logits, phrases """
+        return predict(
+            model=self.model,
+            image=transform(image.convert("RGB"), None)[0],
+            caption=prompt,
+            box_threshold=self.box_thresh,
+            text_threshold=self.text_thresh
+        )
+
+
+if __name__ == "__main__":
+    gdino = GDINO()
+
+    # Inference
+    image = Image.open("assets/color.png")
+    ret = boxes, logits, phrases = gdino(image, "computer. jar. cup")
+    print(ret)
+
+    # Visualize
+    annotated_frame = annotate(image_source=np.asarray(image), boxes=boxes, logits=logits, phrases=phrases)
+    cv2.imwrite("runs/test.jpg", annotated_frame)
