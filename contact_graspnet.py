2a16d7aff945d5bf863e9fdd0ed657df5e7c8659
tongzj <tongzanjia@qq.com>
2025-05-26 21:09:17 +0800

    update

--- contact_graspnet.py
+++ contact_graspnet.py
@@ -14,12 +14,12 @@ class ContactGraspNet:
     def __init__(self,
                  ckpt_dir: Path = Path("checkpoints/contact_graspnet")):
         self.estimator = GraspEstimator(config_utils.load_config(ckpt_dir, batch_size=1))
-        CheckpointIO(checkpoint_dir=ckpt_dir / "checkpoints", model=self.estimator.model).load('model.pt')
+        CheckpointIO(checkpoint_dir=str(ckpt_dir / "checkpoints"), model=self.estimator.model).load('model.pt')
 
     def __call__(self,
                  pcd: np.ndarray):
         """ :param pcd: point cloud with shape (N, 3)
-            :returns: T_grasps_cam [n, 4, 4], scores [n,], contact_pts [n, 3], gripper_openings [n,]"""
+            :returns: T_grasp_cam [n, 4, 4], scores [n,], contact_pts [n, 3], gripper_openings [n,]"""
         return [x[-1] for x in self.estimator.predict_scene_grasps(pcd)]
 
 
